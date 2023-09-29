44327ebf2a2c4025ffaa2316ec18350e679b9094
Satyadev Mishra <iamsatyadevmishra@gmail.com>
2023-09-29 07:58:55 +0000

    js1 done

--- js1/variable.js
+++ js1/variable.js
@@ -1,1 +1,20 @@
-console.log("Radhe Radhe");
+const id=123; //value can not be change once initialised.
+let emailId="satya@google.com";
+var name = "Satyadev";
+age = 24;  //we can also declare a variable directly without using var const and let.
+
+let newName; //undefined and we can declare a variable without initilisition.
+// const id =20;  //single line comment in js to ignore this line for compiler.
+// console.log("Hello world!"); //print single value on console.
+
+// console.log(id);
+
+/* multiline comments
+
+    var--> not to prefer use it due to block 
+    {} and functional scope.
+
+    do prefer to use let and const
+*/
+
+console.table([id,emailId,name,age,newName]); //print all variable value on console in table formate. 
